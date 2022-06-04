2c02a15ebed897e6e563f8816b33724fd4e1f448
Daniel Schaffer <dschaffer@instawork.com>
2022-06-03 17:46:13 -0700

    print the stringified query in the tests

--- django_filtered_relation_coalesce_repro/test/test_job_manager.py
+++ django_filtered_relation_coalesce_repro/test/test_job_manager.py
@@ -6,7 +6,13 @@ from django_filtered_relation_coalesce_repro.models import Job
 class JobManagerTests(TestCase):
 
     def test_with_actual_worker_data_broken(self):
-        list(Job.objects.with_actual_worker_data_broken())
+        qs = Job.objects.with_actual_worker_data_broken()
+        print(str(qs.query))
+
+        list(qs)
 
     def test_with_actual_worker_data_workaround(self):
-        list(Job.objects.with_actual_worker_data_workaround())
+        qs = Job.objects.with_actual_worker_data_workaround()
+        print(str(qs.query))
+
+        list(qs)
